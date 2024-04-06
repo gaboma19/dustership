fab8d0ba54d56f987cd7ae31ea81520ec7885392
Gabriel Ma <gaboma19@gmail.com>
2024-04-06 17:26:14 -0400

    gui assets and cube character

---
+++ scenes/autoload/dialogue_manager.gd
@@ -0,0 +1,56 @@
+extends Node
+
+@onready var text_box_scene = preload("res://scenes/ui/text_box.tscn")
+
+var dialogue_lines: Array[String] = []
+var current_line_index = 0
+
+var text_box
+var text_box_position: Vector2
+
+var sfx: AudioStream
+
+var is_dialogue_active = false
+var can_advance_line = false
+
+
+func start_dialogue(position: Vector2, lines: Array[String], speech_sfx: AudioStream):
+	if is_dialogue_active:
+		return
+		
+	dialogue_lines = lines
+	text_box_position = position
+	sfx = speech_sfx
+	_show_text_box()
+	
+	is_dialogue_active = true
+
+
+func _show_text_box():
+	text_box = text_box_scene.instantiate()
+	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
+	get_tree().root.add_child(text_box)
+	text_box.global_position = text_box_position
+	text_box.display_text(dialogue_lines[current_line_index], sfx)
+	can_advance_line = false
+
+
+func _on_text_box_finished_displaying():
+	can_advance_line = true
+
+
+func _unhandled_input(event):
+	if (
+		event.is_action_pressed("interact") &&
+		is_dialogue_active &&
+		can_advance_line
+	):
+		text_box.queue_free()
+		
+		current_line_index += 1
+		if current_line_index >= dialogue_lines.size():
+			is_dialogue_active = false
+			current_line_index = 0
+			return
+		
+		_show_text_box()
---
+++ scenes/ui/text_box.gd
@@ -0,0 +1,81 @@
+extends MarginContainer
+
+signal finished_displaying
+
+const MAX_WIDTH = 256
+
+var text = ""
+var letter_index = 0
+
+var letter_time = 0.03
+var space_time = 0.06
+var punctuation_time = 0.2
+
+@onready var label = %Label
+@onready var timer = $LetterDisplayTimer
+@onready var audio_player = $AudioStreamPlayer2D
+@onready var next_indicator = %NextIndicator
+
+
+func _ready():
+	scale = Vector2.ZERO
+
+
+func display_text(text_to_display: String, speech_sfx: AudioStream):
+	text = text_to_display
+	label.text = text_to_display
+	audio_player.stream = speech_sfx
+	
+	await resized
+	custom_minimum_size.x = min(size.x, MAX_WIDTH)
+	
+	if size.x > MAX_WIDTH:
+		label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		await resized
+		await resized
+		custom_minimum_size.y = size.y
+		
+	global_position.x -= size.x / 2
+	global_position.y -= size.y + 24
+
+	label.text = ""
+	
+	pivot_offset = Vector2(size.x / 2, size.y)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(
+		self, "scale", Vector2.ONE, 0.15
+	).set_trans(Tween.TRANS_BACK)
+	
+	_display_letter()
+	
+	
+func _display_letter():
+	label.text += text[letter_index]
+	
+	letter_index += 1
+	if letter_index >= text.length():
+		finished_displaying.emit()
+		next_indicator.visible = true
+		return
+		
+	match text[letter_index]:
+		"!", ".", ",", "?":
+			timer.start(punctuation_time)
+		" ":
+			timer.start(space_time)
+		_:
+			timer.start(letter_time)
+			
+			var new_audio_player = audio_player.duplicate()
+			new_audio_player.pitch_scale += randf_range(-0.1, 0.1)
+			if text[letter_index] in ["a", "e", "i", "o", "u"]:
+				new_audio_player.pitch_scale += 0.2
+			get_tree().root.add_child(new_audio_player)
+			new_audio_player.play()
+			await new_audio_player.finished
+			new_audio_player.queue_free()
+
+
+func _on_letter_display_timer_timeout():
+	_display_letter()
