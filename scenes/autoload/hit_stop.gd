4531fd16c61abbf42e3d1aeb8d39d7472b565cd7
Gabriel Ma <gaboma19@gmail.com>
2024-05-02 21:09:49 -0400

    hitstop

---
+++ scenes/autoload/hit_stop.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func short():
+	Engine.time_scale = 0
+	await get_tree().create_timer(0.15, true, false, true).timeout
+	Engine.time_scale = 1
+
+
+func slow_motion_short():
+	Engine.time_scale = 0.5
+	await get_tree().create_timer(0.15, true, false, true).timeout
+	Engine.time_scale = 1
