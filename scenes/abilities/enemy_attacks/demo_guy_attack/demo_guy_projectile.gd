c18b8153afef15756f863375f29a7f8ece46de6c
Troy Chryssos <troychryssos@gmail.com>
2024-05-13 21:04:18 -0400

    projectile flies towards original position of player at attack time

--- scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
+++ scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
@@ -4,15 +4,22 @@ extends StaticBody2D
 
 @onready var hitbox_component = $HitboxComponent
 
+var target: Vector2
+
 
 func _ready():
 	hitbox_component.body_entered.connect(on_body_or_area_entered)
 	hitbox_component.area_entered.connect(on_body_or_area_entered)
+	var player = PartyManager.get_active_member()
+	target = player.global_position
 
 
 func _process(delta):
-	global_position = global_position.move_toward(Vector2.UP, delta * speed)
+	global_position = global_position.move_toward(target, delta * speed)
+	var angle = global_position.angle_to_point(target) + PI/2
+	rotation = angle
 
 
 func on_body_or_area_entered(_body):
-	queue_free()
+	# queue_free()
+	pass
