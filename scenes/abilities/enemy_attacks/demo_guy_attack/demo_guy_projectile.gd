e6eb7679f10ece8e970bed18814a0d4873b71aa4
Troy Chryssos <troychryssos@gmail.com>
2024-05-14 21:00:44 -0400

    trying to get direction based projectiles working

--- scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
+++ scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
@@ -5,19 +5,21 @@ extends StaticBody2D
 @onready var hitbox_component = $HitboxComponent
 
 var target: Vector2
-
+var direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	hitbox_component.body_entered.connect(on_body_or_area_entered)
 	hitbox_component.area_entered.connect(on_body_or_area_entered)
 	var player = PartyManager.get_active_member()
 	target = player.global_position
+	direction = (target - global_position).normalized()
+	var angle = direction.angle() + PI / 2
+	rotation = angle
 
 
 func _process(delta):
-	global_position = global_position.move_toward(target, delta * speed)
-	var angle = global_position.angle_to_point(target) + PI/2
-	rotation = angle
+	# global_position = global_position.move_toward(target, delta * speed)
+	global_position += direction * speed * delta
 
 
 func on_body_or_area_entered(_body):
