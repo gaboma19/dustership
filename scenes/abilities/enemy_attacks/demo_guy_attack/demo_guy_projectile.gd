e114cc3ea1a0b63a876581d4fcfec681175dcbd3
Troy Chryssos <troychryssos@gmail.com>
2024-05-19 13:35:48 -0400

    deletionary skeleton based on demo guy

--- scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
+++ scenes/abilities/enemy_attacks/demo_guy_attack/demo_guy_projectile.gd
@@ -1,27 +1,22 @@
 extends StaticBody2D
 
-@export var speed: float = 200
+@export var speed: float = 150
 
 @onready var hitbox_component = $HitboxComponent
 
-var target: Vector2
 var direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	hitbox_component.body_entered.connect(on_body_or_area_entered)
 	hitbox_component.area_entered.connect(on_body_or_area_entered)
 	var player = PartyManager.get_active_member()
-	target = player.global_position
-	# The direction of the player
-	# direction = (target - global_position).normalized()
-	direction = global_position.direction_to(target)
+	direction = global_position.direction_to(player.global_position)
 	# Calculating the angle of the projectile sprite, not the projectile itself
 	var angle = direction.angle() + PI / 2
 	rotation = angle
 
 
 func _physics_process(delta):
-	# global_position = global_position.move_toward(target, delta * speed)
 	global_position += direction * speed * delta
 
 
