1c8c9f2a8644863cc1a4777dd9055b638aca01e9
Gabriel Ma <gaboma19@gmail.com>
2024-05-19 20:15:03 -0400

    remap level 1

--- scenes/tilemaps/elevation_transition_area/elevation_transition_area.gd
+++ scenes/tilemaps/elevation_transition_area/elevation_transition_area.gd
@@ -1,27 +1,20 @@
 extends Area2D
 
-var enable_tile_set: TileSet = null
-var disable_tile_set: TileSet = null
-
-@export var enable_tile_map: TileMap
-@export var disable_tile_map: TileMap
+@export_enum("One:1", "Two:2") var elevation: int
 
 
 func _ready():
 	body_entered.connect(on_body_entered)
-	
-	if enable_tile_map != null:
-		enable_tile_set = enable_tile_map.tile_set
-	if disable_tile_map != null:
-		disable_tile_set = disable_tile_map.tile_set
 
 
 func on_body_entered(body: Node2D):
-	var player = PartyManager.get_active_member()
-	if player != body:
+	if not body is Player:
 		return
 	
-	if enable_tile_set != null:
-		enable_tile_set.set_physics_layer_collision_layer(1, 0b100000)
-	if disable_tile_set != null:
-		disable_tile_set.set_physics_layer_collision_layer(1, 0)
+	var player = body as Player
+	
+	match elevation:
+		1:
+			player.set_collision_mask_value(6, true)
+		2:
+			player.set_collision_mask_value(6, false)
