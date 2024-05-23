c93444c61e7690cbab7f1d331b1c4bcbde64afa0
Gabriel Ma <gaboma19@gmail.com>
2024-05-23 10:48:52 -0400

    remap level 7

--- scenes/tilemaps/elevation_transition_area/elevation_transition_area.gd
+++ scenes/tilemaps/elevation_transition_area/elevation_transition_area.gd
@@ -3,8 +3,9 @@ extends Area2D
 const floor_1: int = 6
 const floor_2: int = 10
 const floor_3: int = 11
+const floor_4: int = 13
 
-@export_enum("One", "Two", "Three") var elevation: int
+@export_enum("One", "Two", "Three") var elevation: String
 
 
 func _ready():
@@ -18,15 +19,18 @@ func on_body_entered(body: Node2D):
 	var player = body as Player
 	
 	match elevation:
-		0:
+		"One":
 			player.set_collision_mask_value(floor_1, true)
 			player.set_collision_mask_value(floor_2, true)
 			player.set_collision_mask_value(floor_3, false)
-		1:
+			player.set_collision_mask_value(floor_4, false)
+		"Two":
 			player.set_collision_mask_value(floor_1, false)
 			player.set_collision_mask_value(floor_2, true)
 			player.set_collision_mask_value(floor_3, true)
-		2:
+			player.set_collision_mask_value(floor_4, false)
+		"Three":
 			player.set_collision_mask_value(floor_1, false)
 			player.set_collision_mask_value(floor_2, false)
 			player.set_collision_mask_value(floor_3, true)
+			player.set_collision_mask_value(floor_4, true)
