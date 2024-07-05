ef1b4313cb06c17020004c3ec5be944f63f827a3
Gabriel Ma <gaboma19@gmail.com>
2024-07-05 19:34:31 -0400

    fire

--- scenes/entities/enemies/harmonykeeper/harmonykeeper_attack.gd
+++ scenes/entities/enemies/harmonykeeper/harmonykeeper_attack.gd
@@ -13,13 +13,15 @@ func enter(_msg := {}) -> void:
 	enemy.velocity_component.stop()
 	enemy.set_attacking(true)
 	harmonykeeper_rifle_laser.set_casting(true)
+	
+	await get_tree().create_timer(1.0).timeout
+	transition_to_fire()
 
 
 func update(_delta: float) -> void:
 	var aim_vector = get_aiming_vector()
 	
 	harmonykeeper_rifle_laser.aim_direction = aim_vector
-	enemy.update_blend_position(aim_vector)
 
 
 func get_aiming_vector():
@@ -27,7 +29,14 @@ func get_aiming_vector():
 	var target = player.global_position
 	var origin = attack_spawn.global_position
 	
+	# target head, not feet
+	target.y -= 8
+	
 	return origin.direction_to(target)
+
+
+func transition_to_fire():
+	state_machine.transition_to("Fire")
 
 
 func on_attack_range_body_exited(_body: Node2D):
