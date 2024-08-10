21c4d6f8c56b57da59a438462af83821259bb9ed
Gabriel Ma <gaboma19@gmail.com>
2024-08-10 14:14:04 -0400

    tumbleweed

--- scenes/entities/tumbleweed/tumbleweed.gd
+++ scenes/entities/tumbleweed/tumbleweed.gd
@@ -1,2 +1,12 @@
-extends RigidBody2D
+extends Node2D
 
+@onready var rigid_body_2d = $RigidBody2D
+@onready var floor_static_body = $FloorStaticBody
+
+
+func _ready():
+	rigid_body_2d.apply_impulse(Vector2(100, -250))
+
+
+func _physics_process(_delta):
+	floor_static_body.global_position.x = rigid_body_2d.global_position.x
