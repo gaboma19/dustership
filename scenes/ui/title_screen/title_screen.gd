88db5c176683425295906e0ff83cbb82a152de7d
Gabriel Ma <gaboma19@gmail.com>
2024-09-02 17:20:11 -0400

    title screen

--- scenes/ui/title_screen/title_screen.gd
+++ scenes/ui/title_screen/title_screen.gd
@@ -1,11 +1,23 @@
 extends CanvasLayer
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if MaterialsCache.loaded:
 		set_physics_process(false)
 		open()
 
 
+func _unhandled_input(event):
+	if event.is_action_type():
+		get_tree().root.set_input_as_handled()
+		continue_to_main_menu()
+
+
+func continue_to_main_menu():
+	MusicManager.fade_out(5.0)
+	hide()
+	print("continue to main menu")
+
+
 func open():
-	pass
+	MusicManager.play_track("fanfare")
