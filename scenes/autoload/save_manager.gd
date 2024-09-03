f582e9478565082f128c0e3cdfe9be0b613d8efd
Gabriel Ma <gaboma19@gmail.com>
2024-09-03 18:27:36 -0400

    test

--- scenes/autoload/save_manager.gd
+++ scenes/autoload/save_manager.gd
@@ -7,12 +7,41 @@ func save_game(data: SaveData):
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
 	file.store_var(data)
 	file.close()
-	print("Game saved!")
+
+
+func capture_save_data() -> SaveData:
+	var data = SaveData.new()
+	
+	data.items = Inventory.items
+	
+	data.active_layer = OverworldVariables.active_layer
+	data.player_map_position = OverworldVariables.player_map_position
+	data.ingresses = OverworldVariables.ingresses
+	
+	data.member_scenes = PartyManager.member_scenes
+	
+	data.current_health = PlayerVariables.current_health
+	data.max_health = PlayerVariables.max_health
+	data.steel = PlayerVariables.steel
+	data.pause_menu_screen = PlayerVariables.pause_menu_screen
+	data.has_sword = PlayerVariables.has_sword
+	data.has_gun = PlayerVariables.has_gun
+	
+	return data
+
+
+func capture_and_save_game():
+	var data = capture_save_data()
+	save_game(data)
 
 
 func load_game() -> SaveData:
 	var file = FileAccess.open(save_path, FileAccess.READ)
 	var data = file.get_var()
 	file.close()
-	print("Game loaded!")
+	
 	return data
+
+
+func has_save_data() -> bool:
+	return FileAccess.file_exists(save_path)
--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -3,3 +3,5 @@ extends CanvasLayer
 
 func _ready():
 	MusicManager.play_track("heartbeat_of_the_dustership")
+	
+	var data = SaveManager.load_game()
