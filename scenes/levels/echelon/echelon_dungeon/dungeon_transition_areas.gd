01292e86db7d267d230a3fd69de321cf9afea537
Gabriel Ma <gaboma19@gmail.com>
2024-10-04 22:58:58 -0400

    export player positions

--- scenes/levels/echelon/echelon_dungeon/dungeon_transition_areas.gd
+++ scenes/levels/echelon/echelon_dungeon/dungeon_transition_areas.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-@onready var level_transition_area_north: Area2D = $LevelTransitionAreaNorth
-@onready var level_transition_area_south: Area2D = $LevelTransitionAreaSouth
-@onready var level_transition_area_west: Area2D = $LevelTransitionAreaWest
-@onready var level_transition_area_east: Area2D = $LevelTransitionAreaEast
+@export var player_position_north: Vector2
+@export var player_position_south: Vector2
+@export var player_position_west: Vector2
+@export var player_position_east: Vector2
+
+@onready var level_transition_area_north: LevelTransitionArea = $LevelTransitionAreaNorth
+@onready var level_transition_area_south: LevelTransitionArea = $LevelTransitionAreaSouth
+@onready var level_transition_area_west: LevelTransitionArea = $LevelTransitionAreaWest
+@onready var level_transition_area_east: LevelTransitionArea = $LevelTransitionAreaEast
 
 
 func set_doorways(room: Room):
@@ -17,12 +22,16 @@ func set_transition_area_scene_path(neighbor: Room, direction: Vector2i):
 		Vector2i.UP:
 			level_transition_area_north.path = neighbor.scene_path
 			level_transition_area_north.neighbor = neighbor
+			level_transition_area_north.new_player_position = player_position_north
 		Vector2i.DOWN:
 			level_transition_area_south.path = neighbor.scene_path
 			level_transition_area_south.neighbor = neighbor
+			level_transition_area_south.new_player_position = player_position_south
 		Vector2i.LEFT:
 			level_transition_area_west.path = neighbor.scene_path
 			level_transition_area_west.neighbor = neighbor
+			level_transition_area_west.new_player_position = player_position_west
 		Vector2i.RIGHT:
 			level_transition_area_east.path = neighbor.scene_path
 			level_transition_area_east.neighbor = neighbor
+			level_transition_area_east.new_player_position = player_position_east
