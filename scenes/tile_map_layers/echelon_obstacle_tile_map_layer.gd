f08b7976ad130d84e23254b2f883c80d320a36f2
Gabriel Ma <gaboma19@gmail.com>
2024-10-14 20:10:10 -0400

    register obstacles

--- scenes/tile_map_layers/echelon_obstacle_tile_map_layer.gd
+++ scenes/tile_map_layers/echelon_obstacle_tile_map_layer.gd
@@ -1,1 +1,14 @@
 extends TileMapLayer
+
+var obstacles: Array[Obstacle]
+
+
+func _ready():
+	child_entered_tree.connect(on_child_entered_tree)
+
+
+func on_child_entered_tree(child: Node):
+	await child.ready
+	
+	if child is Obstacle:
+		obstacles.append(child)
